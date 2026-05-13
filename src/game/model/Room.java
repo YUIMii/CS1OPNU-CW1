acda183cd773b876a499aaa7859d80268d231450
YUIMii <yuimi142520@gmail.com>
2026-05-13 09:44:18 +0800

    Initial project structure

---
+++ src/game/model/Room.java
@@ -0,0 +1,31 @@
+package game.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Room {
+    private String name;
+    private String description;
+    private Map<String, Room> exits;
+
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.exits = new HashMap<>();
+    }
+
+    public void addExit(String direction, Room room) {
+        exits.put(direction, room);
+    }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    public String getExitsString() {
+        return "Exits: " + exits.keySet().toString();
+    }
+}
